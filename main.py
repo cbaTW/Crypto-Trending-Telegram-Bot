c4d1405f52694592c15dffcf713715e60889e19a
Haku <cbaTW@hotmail.com>
2023-09-09 03:27:13 +0800

    123

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 now = datetime.now()
 
-current_time = now.strftime("%H:%M:%S")
+current_time = now.strftime("%H:%M:%S\r")
 
 f = open('testing.txt', "a")
 f.write(current_time+"\r")
